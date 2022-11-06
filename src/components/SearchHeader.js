3321c99e4449d4b725a6835cb87502cb303b7310
jayanth920 <jayanthgajula92004@gmail.com>
2022-11-06 13:34:13 -0600

    made suitable for deploy

--- src/components/SearchHeader.js
+++ src/components/SearchHeader.js
@@ -5,10 +5,6 @@ import logo from '../giphy-searcher-logo.svg';
 function SearchHeader({ lastSearch }) {
   return (
     <header>
-      <div className="brand">
-        <img src={logo} alt="" />
-         <h1>Giphy Searcher</h1>
-      </div>
       <p className="muted">
         Showing results for <strong>{lastSearch}</strong>
       </p>
